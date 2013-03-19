be8bba039d548a5a8462b147f96a82352d410a7f
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2013-03-19 18:01:08 +0100

    Create service for sonar

--- cookbooks/box/recipes/sonar-plugin.rb
+++ cookbooks/box/recipes/sonar-plugin.rb
@@ -1,5 +1,17 @@
 include_recipe "sonar"
 
+template "/etc/init.d/sonar" do
+    source "sonar/sonar"
+    owner "root"
+    group "root"
+    mode "755"
+    action :create
+end
+
+execute "Launch sonar at boot time" do
+    command "sudo update-rc.d sonar defaults"
+end
+
 execute "Install sonar-php plugin" do
     command "sudo curl -o /opt/sonar/extensions/plugins/sonar-php-plugin.jar http://repository.codehaus.org/org/codehaus/sonar-plugins/php/sonar-php-plugin/1.1/sonar-php-plugin-1.1.jar"
     notifies :restart, resources(:service => "sonar")
@@ -11,3 +23,4 @@ execute "Install Build Breaker plugin" do
     notifies :restart, resources(:service => "sonar")
     not_if "test -f /opt/sonar/extensions/plugins/sonar-build-breaker-plugin.jar"
 end
+
